215fa7afe6eba43b7f6681f6ea0f760c0aaff59a
pju0205 <pju0205@naver.com>
2025-02-04 15:09:12 +0900

    Create PlayerCharacter & Enemy

---
+++ Source/Aura/Private/Character/AuraCharacterBase.cpp
@@ -0,0 +1,18 @@
+// Copyright
+
+
+#include "Character/AuraCharacterBase.h"
+
+AAuraCharacterBase::AAuraCharacterBase()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+}
+
+void AAuraCharacterBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+
