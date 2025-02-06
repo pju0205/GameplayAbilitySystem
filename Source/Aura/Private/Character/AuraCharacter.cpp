6dc0dc785610c0c7a57eebddc1d3bebfa90c50f2
pju0205 <pju0205@naver.com>
2025-02-07 01:44:59 +0900

    GameMode & SpringArm & Camera

--- Source/Aura/Private/Character/AuraCharacter.cpp
+++ Source/Aura/Private/Character/AuraCharacter.cpp
@@ -3,3 +3,19 @@
 
 #include "Character/AuraCharacter.h"
 
+#include "GameFramework/CharacterMovementComponent.h"
+
+AAuraCharacter::AAuraCharacter()
+{
+	GetCharacterMovement()->bOrientRotationToMovement = true;
+	GetCharacterMovement()->RotationRate = FRotator(0.f,400.f,0.f);
+	// 점프 억제
+	GetCharacterMovement()->bConstrainToPlane = true;
+	// 게임 시작 시 평면에서 시작, 평면에서 벗어나 시작하는 것을 방지
+	GetCharacterMovement()->bSnapToPlaneAtStart = true;
+
+	// 마우스(컨트롤러)가 캐릭터의 회전에 영향을 주지 않게 함
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationRoll = false;
+	bUseControllerRotationYaw = false;
+}
